262d85a02652f2823c06b138cb29c584eb4f192f
Benah0987 <wanyoikebenayah77@gmail.com>
2023-06-02 22:18:44 +0300

    has many relationship

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ActiveRecord::Base
   belongs_to :game
+  belongs_to :user
 end
+
+game = Game.first
+# Create a User instance
+user = User.create(name: "Liza")
+# Create a review that belongs to a game and a user
+review = Review.create(score: 8, game_id: game.id, user_id: user.id)
